6509f41d3c4456f1839151a26c946ac159a1c91a
devipilou <devipilou@gmail.com>
2021-10-05 10:25:02 +0200

    nav ok sauf emplacement

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const charmMe = document.querySelectorAll('.charmMe');
 const btnNav = document.querySelector('.navigationIcon');
 const containerMenu = document.querySelector('.containerMenu');
 const b1 = document.querySelector('.b1');
 const b2 = document.querySelector('.b2');
 const b3 = document.querySelector('.b3');
+const menuItem = document.querySelectorAll('.menuItem');
 let toggle = 0;
+
+charming(charmMe);
 
 btnNav.addEventListener('click', () => {
 
@@ -21,3 +25,35 @@ btnNav.addEventListener('click', () => {
         TweenMax.to(containerMenu, 0.5, {top : '-100%', ease: Power2.easeIn})
     }
 })
+
+menuItem.forEach(item => item.addEventListener('mouseenter', (e) => {
+
+    // console.log(e.target.childNodes);
+    let lettreFromItem = Array.from(e.target.childNodes[1].querySelectorAll('span'));
+    // console.log (lettreFromItem);
+
+    for (i = 0; i < lettreFromItem.length; i++){
+        TweenMax.to(lettreFromItem[i],1,{
+            x: Math.floor(Math.random() * 100 - 50),
+            y: Math.floor(Math.random() * 100 - 50),
+            z: Math.floor(Math.random() * 100 - 50),
+            rotation: Math.floor(Math.random() * 100 - 50),
+            opacity: 0.3,
+            ease: Circ.easeOut
+        })
+    }
+
+    menuItem.forEach(item => item.addEventListener('mouseleave', () => {
+        for (k = 0; k < lettreFromItem.length; k++){
+            TweenMax.to(lettreFromItem[k],0.3,{
+                x: 0,
+                y: 0,
+                z: 0,
+                rotation: 0,
+                opacity: 1,
+                ease: Circ.easeIn
+            })
+        }
+    }))
+
+}))
